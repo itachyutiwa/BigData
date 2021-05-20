6a845927882623cd62b07c93de0bb12ca7f26b9d
itachyutiwa <devmail.itachyutiwa@gmail.com>
2021-05-20 11:42:20 +0100

    Question 8, Développement de la fonction qui prend en paramètre une liste de valeur texte et renvoie une nouvelle liste composée uniquement des valeurs qui finissent par "n". Nous avons nommé la fonction fonction1()

---
+++ src/main/scala/ExamenBigData.scala
@@ -0,0 +1,17 @@
+
+object ExamenBigData {
+
+
+//Question 8
+  def fonction1(params:List[String]): Unit ={
+    val maListe = params.filter(f=>f.endsWith("n"))
+    return maListe
+  }
+  }
+
+
+  def main(args:Array[String]):Unit={
+
+
+  }
+}
