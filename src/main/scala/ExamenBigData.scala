589073c2f8cd9254bb86ecc2797a77608da5b35b
itachyutiwa <devmail.itachyutiwa@gmail.com>
2021-05-20 17:09:02 +0100

    Fin de projet

--- src/main/scala/ExamenBigData.scala
+++ src/main/scala/ExamenBigData.scala
@@ -1,3 +1,4 @@
+import scala.collection.immutable.ListMap
 
 object ExamenBigData {
 
@@ -9,25 +10,29 @@ object ExamenBigData {
   }
 
 
-  def liste_double(): Unit ={
-    val maliste = List(
-        ("ecommercemag.fr",null),
-      ("https://www.journalducm.com/contact/","payant"),
-      ("https://www.zatsaz.com/",null),
-      ("https://www.lerevenu.com/",null),
-      ("https://www.cadre-dirigfdeant-magazine.com/","payant"),
-      ("https://www.silicon.fr/services/contact#annoncer","payant"),
-      ("https://www.channelbiz.fr/nous-contacter/",null),
-      ("https://www.itespresso.fr/"->null),
-      ("https://www.industrie-mag.com/article4.html","invité)",
-      ("https://www.jesuisundev.com/article-invite/","invité)",
-      ("https://www.numerama.com/",null)
-      )))
-
-    val maNewListe = maliste.foreach(f=>println(f))
-    println(maNewListe)
+  def avoirdomaine(lien:String) : String ={  //obtention des domaines
+    return lien.replace("https://www.","").split("/")(0)
   }
-  def main(args:Array[String]):Unit={
+
+  def liste_double(): Unit = {
+    val Maliste = List(("ecommercemag.fr", " "),
+      ("https://www.journalducm.com/contact/", "Payant"),
+      ("https://www.zatsaz.com/", " "),
+      ("https://www.lerevenu.com/", " "),
+      ("https://www.cadre-dirigfdeant-magazine.com/", "Payant"),
+      ("https://www.silicon.fr/services/contact#annoner", "Payant"),
+      ("https://www.channelbiz.fr/nous-contacter/", " "),
+      ("https://www.itespresso.fr/", " "),
+      ("https://www.industrie-mag.com/article4.html", "invite"),
+      ("https://www.jesuisundev.com/article-invite/", "invite"),
+      ("https://www.numerama.com/", " ")
+    )
+    // je cree d'abord une liste avec les domaines et ensuite je fais une fusion
+    val finalListe =  (ListMap(Maliste:_*).keys.toList.map(l => avoirdomaine(l) ) zip ListMap(Maliste:_*).values).toList // funsionner les 2 dans une liste de tuple
+    println(finalListe)
+  }
+
+    def main(args:Array[String]):Unit={
 
     println(fonction1(List("Julien","Paul","jean","rac","trec","joel","ed","chris","maurice")))
  println(liste_double())
